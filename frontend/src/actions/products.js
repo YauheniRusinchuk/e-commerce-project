f9a8af10d4ced93b1744d7ce0b2c30d5c6f0651d
YauheniRusinchuk <ruevgal@gmail.com>
2019-09-20 18:50:38 +0300

    add redux functions

---
+++ frontend/src/actions/products.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+import {
+    PRODUCTS_LOADING,
+    PRODUCTS_LOAD
+} from './Actiontypes';
+
+
+// GET Products
+export const getProducts = () => (dispatch, getState) => {
+  axios
+    .get("http://127.0.0.1:8000/api/v1/projects/all")
+    .then(res => {
+        dispatch({type: PRODUCTS_LOADING})
+        dispatch({
+            type: PRODUCTS_LOAD,
+            payload: res.data
+        });
+    })
+    .catch(err =>
+        console.log(err.response)
+     );
+};
---
+++ frontend/src/reducers/products.js
@@ -0,0 +1,26 @@
+import {PRODUCTS_LOADING, PRODUCTS_LOAD} from '../actions/Actiontypes'
+
+
+const initialState = {
+    isLoading: false,
+    products: []
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        case PRODUCTS_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            }
+            break;
+        case PRODUCTS_LOAD:
+            return {
+                ...state,
+                isLoading: false,
+                products: action.payload
+            }
+        default:
+            return state;
+    }
+}
