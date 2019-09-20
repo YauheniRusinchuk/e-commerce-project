9f3091a79d74615ff4fb53e2d5a152000c8ec912
YauheniRusinchuk <ruevgal@gmail.com>
2019-09-21 01:27:18 +0300

    add icon

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import cart from '../assets/shopping-cart.png'
+import home from '../assets/home.png'
 import {Link} from 'react-router-dom'
 
 export const NavBar = () => (
     <div className='header_container'>
-        <Link to='/'>ГЛАВНАЯ</Link>
-        <Link to='/mycard'>МОЯ КОРЗИНА</Link>
+        <div className='header_home'>
+            <Link to='/'><img src={home} alt='home_img' /> ГЛАВНАЯ</Link>
+        </div>
+        <div className='my_cart'>
+            <Link to='/mycard'>МОЯ КОРЗИНА <img src={cart} alt='cart_img' /></Link>
+        </div>
     </div>
 )
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Product({title, photo, price}) {
     return (
         <div className='product_item'>
-            <span className='product_item_price'>{price}</span>
+            <span className='product_item_price'>{price} $</span>
             <img src={photo} alt='photo_watch'/>
             <p className='product_item_title'>{title}</p>
         </div>
