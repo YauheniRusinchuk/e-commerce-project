1796739b69c396e39fc6e2be1f56b0400d1a037d
YauheniRusinchuk <ruevgal@gmail.com>
2019-09-24 14:45:06 +0300

    fixed

--- frontend/src/components/CartView.js
+++ frontend/src/components/CartView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 
 function CartView ({products}) {
@@ -8,7 +8,7 @@ function CartView ({products}) {
             {products.map(product=> {
                 return(
                     <div key={product.id} className='cartview_item'>
-                        <a href="">{product.title}</a>
+                        <Link to={`/detail${product.id}`}>{product.title}</Link>
                         <img src={product.photo} width='30px' height='30px' alt='img_d' />
                         <span>{product.price} $</span>
                         <button>удалить</button>
